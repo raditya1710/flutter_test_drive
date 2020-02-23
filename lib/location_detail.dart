e3595ce18e0a70d66191aa0b2ac47ccc5c0276a9
Alfonsus Raditya Arsadjaja <raditya1710@gmail.com>
2020-02-23 14:32:17 +0700

    lesson 3: creating your own stateless widget

---
+++ lib/location_detail.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class LocationDetail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello, World!"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          _section("One", Colors.red),
+          _section("Two", Colors.green),
+          _section("Three", Colors.blue),
+          _section("Four", Colors.pink),
+        ],
+      ),
+    );
+  }
+
+  Widget _section(String title, Color color) {
+    return Container(
+        decoration: BoxDecoration(
+          color: color,
+        ),
+        child: Text(title));
+  }
+}
