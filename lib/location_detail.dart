d351ee79b067d28f04559d9b3acf1947c194dfb8
Alfonsus Raditya Arsadjaja <raditya1710@gmail.com>
2020-02-23 15:05:50 +0700

    Lesson 04: Organizing Data with Models
    1. Create Model that represent our data
    2. Create a mock model, returning fake data
    3. Update our StatelessWidget to take in a location parameter

--- lib/location_detail.dart
+++ lib/location_detail.dart
@@ -1,30 +1,39 @@
 import 'package:flutter/material.dart';
+import 'models/location.dart';
 
 class LocationDetail extends StatelessWidget {
+  final Location location;
+
+  LocationDetail(this.location);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Hello, World!"),
+        title: Text(location.name),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          _section("One", Colors.red),
-          _section("Two", Colors.green),
-          _section("Three", Colors.blue),
-          _section("Four", Colors.pink),
-        ],
+        children: _renderFacts(location),
       ),
     );
   }
 
-  Widget _section(String title, Color color) {
-    return Container(
-        decoration: BoxDecoration(
-          color: color,
-        ),
-        child: Text(title));
+  List<Widget> _renderFacts(Location location) {
+    var result = List<Widget>();
+    for (int i = 0;i < location.facts.length; i++){
+      result.add(_sectionTitle(location.facts[i].title));
+      result.add(_sectionText(location.facts[i].text));
+    }
+    return result;
+  }
+
+  Widget _sectionTitle(String text){
+    return Text(text);
+  }
+
+  Widget _sectionText(String text){
+    return Text(text);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'location_detail.dart';
+import 'models/location.dart';
+import 'mocks/mock_location.dart';
 
 void main() {
+  final Location mockLocation = MockLocation.FetchAny();
+
   return runApp(MaterialApp(
-    home: LocationDetail(),
+    home: LocationDetail(mockLocation),
   ));
 }
